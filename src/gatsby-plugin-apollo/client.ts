f0965541b5017aeb9c94097005f232c82ce92dbb
Nikita Bondarenko <naradadasosmi@gmail.com>
2023-05-19 10:23:45 +0300

    commit

--- src/gatsby-plugin-apollo/client.ts
+++ src/gatsby-plugin-apollo/client.ts
@@ -4,7 +4,7 @@ import {ApolloClient, HttpLink, InMemoryCache} from '@apollo/client';
 const client = new ApolloClient({
     cache: new InMemoryCache(),
     link: new HttpLink({
-        uri: 'https://angel-back.testingplace.ru/graphql/',
+        uri: 'http://admin.angelclub.ru/graphql',
         fetch
     })
 });
