04c82f55db495d50f7d96a028f7d068cd217857d
Nikita Bondarenko <naradadasosmi@gmail.com>
2023-07-26 13:15:08 +0300

    first commit

--- init.sh
+++ init.sh
@@ -2,5 +2,5 @@ git init
 git add .
 git commit -m "first commit"
 git branch -M main
-git remote add origin https://github.com/nikita-bondarenko/angelclub_evacuator.git
+git remote add origin https://github.com/cheap-and-best/evacuator.git
 git push -u origin main
